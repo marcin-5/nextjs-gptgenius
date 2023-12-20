bc6a3bc72512f5c2d5d3fd5725187b9accb12a85
Marcin Bojara <marcin_5@mail.ru>
2023-12-20 14:42:43 +0100

    add sidebar header component

---
+++ app/components/SidebarHeader.jsx
@@ -0,0 +1,14 @@
+import { SiOpenaigym } from 'react-icons/si';
+import ThemeToggle from './ThemeToggle';
+
+const SidebarHeader = () => {
+  return (
+    <div className='flex gap-4 items-center px-4 mb-4'>
+      <SiOpenaigym className='w-10 h-10 text-primary' />
+      <h2 className='font-extrabold test-xl text-primary'>GPTGenius</h2>
+      <ThemeToggle />
+    </div>
+  );
+};
+
+export default SidebarHeader;
