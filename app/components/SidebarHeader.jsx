3a4baac7872f593014c343f57cedcdfe10dbd435
Marcin Bojara <marcin_5@mail.ru>
2023-12-20 20:51:35 +0100

    handle toggle theme button

--- app/components/SidebarHeader.jsx
+++ app/components/SidebarHeader.jsx
@@ -5,7 +5,7 @@ const SidebarHeader = () => {
   return (
     <div className='flex gap-4 items-center px-4 mb-4'>
       <SiOpenaigym className='w-10 h-10 text-primary' />
-      <h2 className='font-extrabold test-xl text-primary'>GPTGenius</h2>
+      <h2 className='mr-auto font-extrabold test-xl text-primary'>GPTGenius</h2>
       <ThemeToggle />
     </div>
   );
--- app/components/ThemeToggle.jsx
+++ app/components/ThemeToggle.jsx
@@ -1,5 +1,29 @@
+'use client';
+import { useState } from 'react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '../../tailwind.config.js';
+
 const ThemeToggle = () => {
-  return <button className='btn btn-primary btn-sm'>ThemeToggle</button>;
+  const { daisyui } = resolveConfig(tailwindConfig);
+  const [theme, setTheme] = useState(daisyui.themes[0]);
+
+  const toggleTheme = () => {
+    const newTheme =
+      theme === daisyui.themes[0] ? daisyui.themes[1] : daisyui.themes[0];
+    setTheme(newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme} className='btn btn-sm btn-outline'>
+      {theme === daisyui.themes[0] ? (
+        <BsMoonFill className='w-4 h-4' />
+      ) : (
+        <BsSunFill className='w-4 h-4' />
+      )}
+    </button>
+  );
 };
 
 export default ThemeToggle;
