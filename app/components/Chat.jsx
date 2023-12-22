815619c053a1af82520b937acf09a632705218f1
Marcin Bojara <marcin_5@mail.ru>
2023-12-22 13:20:43 +0100

    add chat component

---
+++ app/components/Chat.jsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useState } from 'react';
+
+const Chat = () => {
+  const [text, setText] = useState('');
+  const [message, setMessage] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  };
+
+  return (
+    <div className='min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]'>
+      <div>
+        <h2 className='text-5xl'>messages</h2>
+      </div>
+      <form onSubmit={handleSubmit} className='pt-12 max-w-4xl'>
+        <div className='w-full join'>
+          <input
+            type='text'
+            placeholder='Message GeniusGPT'
+            className='w-full input input-bordered join-item'
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+            required
+          />
+          <button className='btn btn-primary join-item' type='submit'>
+            ask question
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
