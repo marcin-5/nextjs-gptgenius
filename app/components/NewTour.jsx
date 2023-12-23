a9ec01c5ebad64e91516011fc973131b0b53c92e
Marcin Bojara <marcin_5@mail.ru>
2023-12-23 14:05:25 +0100

    add form to new tour component

--- app/components/NewTour.jsx
+++ app/components/NewTour.jsx
@@ -1,7 +1,43 @@
-const NewTour = () => {
-  return (
-    <div>NewTour</div>
-  )
-}
+'use client';
 
-export default NewTour
+import TourInfo from './TourInfo';
+
+const NewTour = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    console.log(destination);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='max-w-2xl'>
+        <h2 className='mb-4'>Select your dream destination</h2>
+        <div className='w-full join'>
+          <input
+            type='text'
+            className='w-full input input-bordered join-item'
+            placeholder='city'
+            name='city'
+            required
+          />
+          <input
+            type='text'
+            className='w-full input input-bordered join-item'
+            placeholder='country'
+            name='country'
+            required
+          />
+          <button className='btn btn-primary join-item' type='submit'>
+            generate tour
+          </button>
+        </div>
+      </form>
+      <div className='mt-16'>
+        <TourInfo />
+      </div>
+    </>
+  );
+};
+
+export default NewTour;
