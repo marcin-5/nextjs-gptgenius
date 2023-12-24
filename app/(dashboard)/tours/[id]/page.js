f60a3abf6b1a6e4499f604ede33f423391fa1800
Marcin Bojara <marcin_5@mail.ru>
2023-12-24 16:25:30 +0100

    generate image

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -1,5 +1,6 @@
 import TourInfo from '@/app/components/TourInfo';
-import { getSingleTour } from '@/utils/action';
+import { generateTourImage, getSingleTour } from '@/utils/action';
+import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
@@ -7,11 +8,28 @@ const SingleTourPage = async ({ params }) => {
   const tour = await getSingleTour(params.id);
   if (!tour) redirect('/tours');
 
+  const tourImage = await generateTourImage({
+    city: tour.city,
+    country: tour.country,
+  });
+
   return (
     <div>
       <Link href='/tours' className='mb-12 btn btn-secondary'>
         back to tours
       </Link>
+      {tourImage ? (
+        <div>
+          <Image
+            src={tourImage}
+            width={300}
+            height={300}
+            className='object-cover mb-16 w-96 h-96 rounded-xl shadow-xl'
+            alt={tour.title}
+            priority
+          />
+        </div>
+      ) : null}
       <TourInfo tour={tour} />
     </div>
   );
