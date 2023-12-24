0cd66e62bfdbae35a4c99a3d1be222bf9ec02c60
Marcin Bojara <marcin_5@mail.ru>
2023-12-24 13:39:49 +0100

    add search functionality

--- app/components/ToursPage.jsx
+++ app/components/ToursPage.jsx
@@ -2,15 +2,37 @@
 
 import { getAllTours } from '@/utils/action';
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import ToursList from './ToursList';
 
 const ToursPage = () => {
+  const [searchValue, setSearchValue] = useState('');
   const { data, isPending } = useQuery({
-    queryKey: ['tours'],
-    queryFn: () => getAllTours(),
+    queryKey: ['tours', searchValue],
+    queryFn: () => getAllTours(searchValue),
   });
   return (
     <>
+      <form className='mb-12 max-w-lg'>
+        <div className='join w-fill'>
+          <input
+            type='text'
+            placeholder='enter city or country here...'
+            className='w-full input input-bordered join-item'
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            required
+          />
+          <button
+            className='btn btn-primary join-item'
+            type='button'
+            disabled={isPending}
+            onClick={() => setSearchValue('')}
+          >
+            {isPending ? 'please wait...' : 'reset'}
+          </button>
+        </div>
+      </form>
       {isPending ? (
         <span className='loading'></span>
       ) : (
