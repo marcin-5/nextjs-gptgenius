e59f2316095339aa7766695b964fe3936e4277d4
Marcin Bojara <marcin_5@mail.ru>
2023-12-24 16:48:01 +0100

    get images from unsplash instead of openai

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -1,17 +1,24 @@
 import TourInfo from '@/app/components/TourInfo';
 import { generateTourImage, getSingleTour } from '@/utils/action';
+import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
 
 const SingleTourPage = async ({ params }) => {
   const tour = await getSingleTour(params.id);
   if (!tour) redirect('/tours');
 
-  const tourImage = await generateTourImage({
-    city: tour.city,
-    country: tour.country,
-  });
+  const { data } = await axios(`${url}${tour.city}`);
+  const tourImage = data?.results[0]?.urls?.raw;
+  
+  // openAI image
+  // const tourImage = await generateTourImage({
+  //   city: tour.city,
+  //   country: tour.country,
+  // });
 
   return (
     <div>
