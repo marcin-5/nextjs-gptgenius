ab02b0a1f79fc3de286178f5063db8794ff1db5e
Marcin Bojara <marcin_5@mail.ru>
2023-12-25 23:08:11 +0100

    display tokens

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,7 +1,20 @@
-import { UserProfile } from '@clerk/nextjs';
+import { fetchUserTokensByEmail } from '@/utils/action';
+import { UserProfile, currentUser } from '@clerk/nextjs';
 
-const ProfilePage = () => {
-  return <UserProfile />;
+const ProfilePage = async () => {
+  const user = await currentUser();
+  const currentTokens = await fetchUserTokensByEmail(
+    user.emailAddresses[0].emailAddress
+  );
+
+  return (
+    <div>
+      <h2 className='mb-8 ml-8 font-extrabold trxt-xl'>
+        Token Amount: {currentTokens}
+      </h2>
+      <UserProfile />
+    </div>
+  );
 };
 
 export default ProfilePage;
